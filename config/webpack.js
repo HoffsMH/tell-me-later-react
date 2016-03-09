bc2aa43884837af9bd3f9655cc6fc45514c48b75
HoffsMH <hecker.matthew@gmail.com>
2016-03-08 17:58:11 -0700

    This adds initial dom structure and styles to our react elements
    Also:
    implements initial build process for webpack
    
    from here we have
      * determined what files need to be poured into our rails app
      * determined file structure and wiring for our react components

---
+++ config/webpack.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '../src/index.js')
+    // test: "./app/assets/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: "[name]-bundle.min.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel', query: {presets: ['react', 'es2015']}  },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader") },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+      { test: /.(woff|woff2)$/, loader: "file-loader" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      {test: /\.png$/,  loader: "url?limit=10000&mimetype=image/png" },
+    ]
+  },
+  plugins: [
+        new ExtractTextPlugin("./assets/stylesheets/[name]-bundle.css")
+  ],
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', 'css']
+  }
+};
