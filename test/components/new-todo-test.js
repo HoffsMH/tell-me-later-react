6efdcc571574f0a279205ee8d2f0b34d412f9fcc
HoffsMH <hecker.matthew@gmail.com>
2016-03-26 16:41:10 -0600

    our first meaningful and concise react component test!

--- test/components/new-todo-test.js
+++ test/components/new-todo-test.js
@@ -2,12 +2,25 @@ const NewTodo   = require("../../src/app/components/new-todo/new-todo.js");
 const React     = require("react");
 const enzyme    = require("enzyme");
 const mount     = enzyme.mount;
+const sinon     = require("sinon");
 
 const chai  = require("chai");
 const expect = chai.expect;
 
-describe("new-todo component", function()  {
-  it('allows us to set props', () => {
-    const wrapper = mount(<NewTodo title="test" />);
+describe("new-todo component", () =>  {
+  context("when editing the title field", () => {
+    before("each", () => {
+      sinon.spy(NewTodo.prototype.__reactAutoBindMap, "handleTitleChange");
+    });
+    it("calls handleTitleChange", () => {
+      var nullfunc = function() {return null;};
+      const wrapper = mount(<NewTodo title="asdf" handleUpdate={nullfunc}/>);
+
+
+      wrapper.find("input.title").node.value = "tacos";
+      wrapper.find("input.title").simulate("change");
+
+      expect(NewTodo.prototype.__reactAutoBindMap.handleTitleChange.called).to.be.true
+    });
   });
 });
