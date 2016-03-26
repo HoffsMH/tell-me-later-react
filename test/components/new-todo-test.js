31d8460e8549697ff40264c987a932c94ae95e2c
HoffsMH <hecker.matthew@gmail.com>
2016-03-26 17:27:28 -0600

    adds react component test for showtime

--- test/components/new-todo-test.js
+++ test/components/new-todo-test.js
@@ -1,26 +1,56 @@
-const NewTodo   = require("../../src/app/components/new-todo/new-todo.js");
-const React     = require("react");
-const enzyme    = require("enzyme");
-const mount     = enzyme.mount;
-const sinon     = require("sinon");
+const TestHelper = require("../test-helper.js");
+const NewTodo    = require("../../src/app/components/new-todo/new-todo.js");
+const React      = require("react");
+const mount      = require("enzyme").mount;
+const sinon      = require("sinon");
 
-const chai  = require("chai");
-const expect = chai.expect;
+const expect     = require("chai").expect;
+
+const NewTodoMethods = TestHelper.getMethods(NewTodo);
 
 describe("new-todo component", () =>  {
   context("when editing the title field", () => {
     before("each", () => {
-      sinon.spy(NewTodo.prototype.__reactAutoBindMap, "handleTitleChange");
+      sinon.spy(NewTodoMethods, "handleTitleChange");
     });
     it("calls handleTitleChange", () => {
-      var nullfunc = function() {return null;};
-      const wrapper = mount(<NewTodo title="asdf" handleUpdate={nullfunc}/>);
+      const wrapper = mount(<NewTodo title="test" handleUpdate={TestHelper.noop}/>);
 
-
-      wrapper.find("input.title").node.value = "tacos";
       wrapper.find("input.title").simulate("change");
 
-      expect(NewTodo.prototype.__reactAutoBindMap.handleTitleChange.called).to.be.true
+      expect(NewTodoMethods.handleTitleChange.called).to.eq(true);
+    });
+    it("updates state", () => {
+      const wrapper = mount(<NewTodo title="old value" handleUpdate={TestHelper.noop}/>);
+
+      wrapper.find("input.title").node.value = "new value";
+      wrapper.find("input.title").simulate("change");
+
+      expect(wrapper.state().title).to.eq("new value");
+    });
+  });
+  context("when editing the showtime field", () => {
+    before("each", () => {
+      sinon.spy(NewTodoMethods, "handleShowTimeChange");
+    });
+    it("calls handleShowTimeChange", () => {
+      const wrapper = mount(<NewTodo title="test"
+                                     showTime="test"
+                                     handleUpdate={TestHelper.noop}/>);
+
+      wrapper.find("input.show-time").simulate("change");
+
+      expect(NewTodoMethods.handleShowTimeChange.called).to.eq(true);
+    });
+    it("updates state", () => {
+      const wrapper = mount(<NewTodo title="old value"
+                                      showTime="test"
+                                      handleUpdate={TestHelper.noop}/>);
+
+      wrapper.find("input.show-time").node.value = "new value";
+      wrapper.find("input.show-time").simulate("change");
+
+      expect(wrapper.state().showTime).to.eq("new value");
     });
   });
 });
--- test/test-helper.js
+++ test/test-helper.js
@@ -5,3 +5,10 @@ global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 
 global.window = document.defaultView;
 global.navigator = {userAgent: 'node.js'};
+
+module.exports = {
+  noop() {return null;},
+  getMethods(reactClass) {
+    return reactClass.prototype.__reactAutoBindMap
+  }
+};
